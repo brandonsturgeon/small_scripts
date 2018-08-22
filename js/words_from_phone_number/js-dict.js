96462207b56c3fec54b06238f78413488793a2cb
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-08-22 11:48:12 -0700

    Add words_from_phone_number

---
+++ js/words_from_phone_number/js-dict.js
@@ -0,0 +1,47 @@
+const fs = require("fs")
+
+const dictionary = fs.readFileSync("words.txt", "utf8")
+
+const numberMap = {
+  1: [],
+  2: ["A","B","C"],
+  3: ["D", "E", "F"],
+  4: ["G", "H", "I"],
+  5: ["J", "K", "L"],
+  6: ["M", "N", "O"],
+  7: ["P", "Q", "R", "S"],
+  8: ["T", "U", "V"],
+  9: ["W", "X", "Y", "Z"],
+  0: []
+}
+
+const number = "669"
+
+const generatePatternForDigit = d => `(${numberMap[d].join("|")})`
+
+const generatePatternForNumber = number => new RegExp(`^${number.split("").map(d => generatePatternForDigit(d)).join("")}$`, "gmi")
+
+console.log(`Phone number: ${number}`)
+const pattern = generatePatternForNumber(number)
+console.log(pattern)
+
+console.log(dictionary.match(pattern))
+
+
+/*
+ Example Runs:
+
+Phone number: 7378423
+/^(P|Q|R|S)(D|E|F)(P|Q|R|S)(T|U|V)(G|H|I)(A|B|C)(D|E|F)$/gim
+
+              |
+              v
+[ 'restiad', 'Service', 'sestiad' ]
+
+Phone number: 669
+/^(M|N|O)(M|N|O)(W|X|Y|Z)$/gim
+[ 'MMW', 'MMX', 'moy', 'mow', 'NNW', 'NNX', 'noy', 'now', 'Nox', 'ony' ]
+                                                    ^
+                                                    |
+*/
+
