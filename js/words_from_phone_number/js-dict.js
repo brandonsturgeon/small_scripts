cbb2c11727966151cbfbbb33ae17aadd9a0f6a95
Brandon Sturgeon <sturgeonb4@gmail.com>
2018-08-24 19:27:20 -0700

    Add permutations and benchmarking

--- js/words_from_phone_number/js-dict.js
+++ js/words_from_phone_number/js-dict.js
@@ -1,10 +1,12 @@
 const fs = require("fs")
 
-const dictionary = fs.readFileSync("words.txt", "utf8")
+const regexDictionary = fs.readFileSync("words.txt", "utf8")
+const dictionary = {}
+regexDictionary.split("\n").map(word => dictionary[word.toUpperCase()] = word)
 
 const numberMap = {
   1: [],
-  2: ["A","B","C"],
+  2: ["A", "B", "C"],
   3: ["D", "E", "F"],
   4: ["G", "H", "I"],
   5: ["J", "K", "L"],
@@ -15,18 +17,18 @@ const numberMap = {
   0: []
 }
 
-const number = "669"
+const funWay = number => {
+  const generatePatternForDigit = d => `(${numberMap[d].join("|")})`
+  const generatePatternForDigits = digits => digits.split("").map(d => generatePatternForDigit(d)).join("")
+  const generatePatternForNumber = number => new RegExp(`^${generatePatternForDigits(number)}$`, "gmi")
 
-const generatePatternForDigit = d => `(${numberMap[d].join("|")})`
-const generatePatternForDigits = digits => digits.split("").map(d => generatePatternForDigit(d)).join("")
-const generatePatternForNumber = number => new RegExp(`^${generatePatternForDigits(number)}$`, "gmi")
+  const pattern = generatePatternForNumber(number)
 
-console.log(`Phone number: ${number}`)
-const pattern = generatePatternForNumber(number)
-console.log(pattern)
+  return regexDictionary.match(pattern)
+}
 
-console.log(dictionary.match(pattern))
-
+funWay("669")
+funWay("7378423")
 
 /*
  Example Runs:
@@ -45,3 +47,72 @@ Phone number: 669
                                                     |
 */
 
+const otherWay = number => {
+
+
+  let stripped = number.replace("1", "")
+  stripped = stripped.replace("0", "")
+
+  let possibleWords = []
+
+  for (let digit of stripped) {
+    const possibleLetters = numberMap[digit]
+
+    if ( possibleWords.length === 0 ) {
+      possibleWords = possibleLetters
+    } else {
+      const newWords = []
+
+      for (let possibleWord of possibleWords) {
+        for (let possibleLetter of possibleLetters)  {
+          newWords.push(possibleWord + possibleLetter)
+        }
+      }
+
+      possibleWords = newWords
+    }
+  };
+
+
+  const dictionaryWords = possibleWords.map(word => dictionary[word])
+  const onlyGoodWords = dictionaryWords.filter(w => w)
+
+  return onlyGoodWords
+}
+
+const benchmark = (runs, func, runArg) => {
+
+  const startTime = new Date().getTime()
+
+  for (let run of Array(runs)) {
+    func(runArg)
+  }
+
+  const endTime = new Date().getTime()
+  const runTime = endTime - startTime
+
+  console.log(`[${runArg}] Average: ${runTime/runs}ms`)
+}
+
+const largerExampleNumber = "7378423"
+const smallerExampleNumber = "669"
+
+console.log("Permutations:")
+benchmark(1000, otherWay, largerExampleNumber)
+benchmark(1000, otherWay, smallerExampleNumber)
+
+console.log("Regex:")
+benchmark(1000, funWay, largerExampleNumber)
+benchmark(1000, funWay, smallerExampleNumber)
+
+/*
+
+Permutations:
+[7378423] Average: 1.711ms
+[669] Average: 0.008ms
+
+Regex:
+[7378423] Average: 3.26ms
+[669] Average: 5.499ms
+
+*/
